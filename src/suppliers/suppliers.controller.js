585618ff2989085aade98241f257e6b80724a3f7
Vanessa <zdperfectx@gmail.com>
2023-12-01 17:16:19 -0600

    posted suppliers using service and controller

--- src/suppliers/suppliers.controller.js
+++ src/suppliers/suppliers.controller.js
@@ -1,5 +1,41 @@
-async function create(req, res, next) {
-  res.status(201).json({ data: { supplier_name: "new supplier" } });
+const suppliersService = require("./suppliers.service");
+const hasProperties = require("../errors/hasProperties");
+const suppliers = require("../db/fixtures/suppliers");
+
+const hasRequiredProperties = hasProperties("supplier_name", "supplier_email");
+
+// Validity for the create function 
+const VALID_PROPERTIES = [
+  "supplier_name",
+  "supplier_address_line_1",
+  "supplier_address_line_2",
+  "supplier_city",
+  "supplier_state",
+  "supplier_zip",
+  "supplier_phone",
+  "supplier_email",
+  "supplier_notes",
+  "supplier_type_of_goods",
+]
+
+function hasOnlyValidProperties(req, res, next) {
+  const { data = {} } = req.body;
+  const invalidFields = Object.keys(data).filter((field) => !VALID_PROPERTIES.includes(field));
+
+  if (invalidFields.length) {
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  }
+  next();
+}
+
+function create(req, res, next) {
+  suppliersService
+  .create(req.body.data)
+  .then((data) => res.status(201).json({ data }))
+  .catch(next);
 }
 
 async function update(req, res, next) {
@@ -11,7 +47,7 @@ async function destroy(req, res, next) {
 }
 
 module.exports = {
-  create,
+  create: [hasOnlyValidProperties, hasRequiredProperties, create],
   update,
   delete: destroy,
 };
